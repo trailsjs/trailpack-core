ec36a8202b4a4b0c5159e9d50aab9e5988020005
Travis Webb <me@traviswebb.com>
2016-02-14 04:02:50 -0500

    [test] add tests for getClassMethods static properties

--- test/lib/context.test.js
+++ test/lib/context.test.js
@@ -12,18 +12,34 @@ describe('lib.Context', () => {
     const B = class B extends A {
       bar () { }
     }
+    const C = class B extends A {
+      bar () { }
+      baz () { }
+      get getter () {
+        return 'getter'
+      }
+      static staticThing () { }
+    }
     it('should return class methods for object', () => {
-      const methods = lib.Context.getClassMethods(new A())
+      const methods = lib.Context.getClassMethods(new A(), A)
 
       assert.equal(methods.length, 1)
       assert.equal(methods[0], 'foo')
     })
     it('should return class methods for all objects in prototype chain', () => {
-      const methods = lib.Context.getClassMethods(new B())
+      const methods = lib.Context.getClassMethods(new B(), B)
 
       assert.equal(methods.length, 2)
       assert(_.includes(methods, 'foo'))
       assert(_.includes(methods, 'bar'))
+    })
+    it('should return only *instance methods* and no other type of thing', () => {
+      const methods = lib.Context.getClassMethods(new C(), C)
+
+      assert.equal(methods.length, 3)
+      assert(_.includes(methods, 'bar'))
+      assert(_.includes(methods, 'foo'))
+      assert(_.includes(methods, 'baz'))
     })
   })
 })
